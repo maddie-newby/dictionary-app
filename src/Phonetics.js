940d4e333ed9e0c506ba5484cb2ec79c90c37738
maddie-newby <madison1999@hotmail.co.uk>
2023-08-11 14:35:51 +0100

    Add phonetic audio rule

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
-  if (props.phonetics && props.phonetics.text) {
+  if (props.phonetics.audio && props.phonetics.text) {
     return (
       <div className="PhoneticLine">
         <h2 className="Phonetic">{props.phonetics.text}</h2>
         <button className="PlayLink">
           <ReactAudioPlayer src={props.phonetics.audio} controls />
         </button>
+      </div>
+    );
+  }
+  if (props.phonetics.text && !props.phonetics.audio) {
+    return (
+      <div className="PhoneticLine">
+        <h2 className="Phonetic mt-2">{props.phonetics.text}</h2>
       </div>
     );
   } else {
